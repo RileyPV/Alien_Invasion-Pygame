77798c920d7d8c761c50d115a6ba6f818352fd35
Riley Richards <riley_richards1@baylor.edu>
2022-02-20 12:45:49 -0600

    Created ship class and created an instance of the ship in the alien invasion program. Need to debug image files.

--- ship.py
+++ ship.py
@@ -4,7 +4,7 @@ class Ship:
     #A class to manage the ship.
     def __init__(self, ai_game):
         #initialize the ship and set its starting position
-        self.screen = ai.game.screen
+        self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship image and its rect.

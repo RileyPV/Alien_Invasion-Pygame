0b85774c7102441aa1db98728ee196547380b6ea
Riley Richards <riley_richards1@baylor.edu>
2022-02-23 15:19:41 -0600

    added bullet settings and created bullet class and initialized

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from turtle import width
+
+
 class Settings:
     #A class to store all settings for Alien Invasion
     def __init__(self):
@@ -9,3 +12,9 @@ class Settings:
 
         #Ship settings
         self.ship_speed = 1.5
+
+        #Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)

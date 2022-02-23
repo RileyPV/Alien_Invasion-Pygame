ea6cddc737cb869a3eb5f2fd82ea64c8882d4bd5
Riley Richards <riley_richards1@baylor.edu>
2022-02-23 12:16:02 -0600

    Created Methods and updated code to move the ship left and right with key presses

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,6 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-
         self.screen = pygame.display.set_mode ((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
@@ -24,12 +23,28 @@ class AlienInvasion:
         #Start the main loop for the game.
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
+
     def _check_events(self):
         #Respond to keypresses and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    sys.exit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                    #Move the ship to the right
+                    #self.ship.rect.x += 1
+
     def _update_screen(self):
         #Update images on the screen, and flip to the new screen.
         self.screen.fill(self.settings.bg_color)

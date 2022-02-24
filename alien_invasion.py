d43f0ee1463393d1b1e7b834589261fcba7cc3d8
Riley Richards <riley_richards1@baylor.edu>
2022-02-24 16:13:58 -0600

    Created an instance of the button and called the draw button method at the beginning of the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -28,6 +29,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        #Make the play button
+        self.play_button = Button(self, "Play")
 
         #Set the background color
         self.bg_color = (230, 230, 230)
@@ -83,6 +87,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        #Draw the play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()

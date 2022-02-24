c29e0d6f9f4123941b39fb7e1ae3393d177dfe21
Riley Richards <riley_richards1@baylor.edu>
2022-02-24 17:17:32 -0600

    Debugging mouse visibility

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,7 +62,7 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         #Start a new game when the player clicks Play
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
-        if button_clicked and self.stats.game_active:
+        if button_clicked and not self.stats.game_active:
             #Hide the mouse cursor
             pygame.mouse.set_visible(False)
             #Reset the game statistics
